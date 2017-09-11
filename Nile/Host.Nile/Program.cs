1f638f963f4cd2649839ac6db31b3b5eadf6c7dc
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-11 17:31:09 -0500

    Host.Nile

---
+++ Nile/Host.Nile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host.Nile
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            // add, list, alist
+
+        }
+        static void DisplayMenu (string[] args) // modifier should be static  
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+      
+}
