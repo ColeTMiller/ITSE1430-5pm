82ad8e0d2a2cd83441a15cca547d831d69d4ce40
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-11 19:03:40 -0500

    Host.Nile

--- Nile/Host.Nile/Program.cs
+++ Nile/Host.Nile/Program.cs
@@ -10,31 +10,90 @@ namespace Host.Nile
     {
         static void Main( string[] args )
         {
-            // add, list, alist
-            GetInput();
+            bool quit = false;
+            do
+            {
+                // add, list, alist
+                char choice = GetInput();
+                switch (choice)
+                {
+               
+                    case 'A':
+                    AddProduct();
+                    break;
 
+                    case 'L':
+                    ListProduct();
+                    break;
+
+                    case 'Q': quit = true; break; 
+                    
+                };
+            } while (!quit);  
+        }
+
+        private static void AddProduct()
+        {
+            Console.Write("Enter product name: ");
+            Name = Console.ReadLine().Trim();
+
+            // Ensure not empty
+
+            Console.Write("Enter price (>0): ");
+            Price = Console.ReadLine();
+
+            Console.Write("Enter optional description: ");
+            productDescription= Console.ReadLine().Trim();
+
+            Console.WriteLine("Is it discontinued (Y/N): ");
+            bool discontinued = Console.ReadLine().Trim(); 
+        }
+        private static void ListProduct()
+        {
+            throw new NotImplementedException();
         }
         static char GetInput() // modifier should be static  // void has no return type 
         {
             while (true)
-            { 
-            Console.WriteLine("A)dd Product");
-            Console.WriteLine("L)ist Products");
-            Console.WriteLine("Q)uit");
+            {
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("".PadLeft(10,));
+                  Console.WriteLine("A)dd Product");
+                  Console.WriteLine("L)ist Products");
+                  Console.WriteLine("Q)uit");
 
 
-            string input = Console.ReadLine();
-            if (input == "A")
-                return 'A';
-            else if (input == "L")
-                return 'L';
-            else if (input == "Q")
-                return 'Q';
+                string input = Console.ReadLine();                      
+                   // option 1 = string literal
+                  // if (input !=" ")
 
+                 // option 2 - string field
+                 // if (input !=string.Empty)
+
+                //option 3 - length does not solve null
+                if (input!= null && input.Length !=0)
+                {
+                    // string comparion
+                    if (string.Compare(input, "A", true) == 0)
+                        return 'A';
+                    //char comparison
+                    else if (letter == "L")
+                        return 'L';
+                    else if (letter == "Q")
+                        return 'Q';
+                }
                 //Error
                 Console.WriteLine("Please choose a valid option");
             }
         }
+
+        // product goes
+        static string  Name;
+        static decimal Price;
+        static string  Description;
+        static bool    Discontinued; 
+        
     }
+
       
 }
