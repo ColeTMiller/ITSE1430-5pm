8f39160b86bbf9fa71ba3a1d26499e685577b1e1
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-12 13:26:28 -0500

    update

--- lab/lab_Cole_Miller/Program.cs
+++ lab/lab_Cole_Miller/Program.cs
@@ -35,17 +35,31 @@ namespace lab_Cole_Miller
 
         private static void DeleteMovie()
         {
-            ;
-        }
 
+            Name = Name.Remove(Name,Description);
+        }
         private static void ListMovie()
         {
-            ;
+            Console.WriteLine(Name);
+            Console.WriteLine(Description);
+            Console.WriteLine(Time);
+            Console.WriteLine(Own);
         }
 
         private static void AddMovie()
         {
-            ;
+            Console.WriteLine("Enter Movie Name:");
+            Name = Console.ReadLine().Trim();
+
+            Console.WriteLine("Enter description of Movie:");
+            Description = Console.ReadLine().Trim();
+
+            Console.WriteLine("Enter how long it is:");
+            Time = Console.ReadLine().Trim();
+
+            Console.WriteLine("Do you own this movie:");
+            Own = Console.ReadLine().Trim(); 
+
         }
 
         static char MovieSelection()
@@ -59,22 +73,28 @@ namespace lab_Cole_Miller
                 Console.WriteLine("4. Quit Program");
 
                 string input = Console.ReadLine();
-                if (input !=null && input.Length !=0)
+                if (input != null && input.Length != 0)
                 {
-                    if (string.Compare(input, '1', true) == 0)
-                        return AddMovie();
-                    if (input == '1')
-                        return AddMovie();
-                    else if (input == '2')
-                        return ListMovies();
-                    else if (input == '3')
-                        return DeleteMovie();
-
+                    if (string.Compare(input, "1", true) == 0)
+                        return '1';
+                    else if (string.Compare(input, "2", true) == 0)
+                        return '2';
+                    else if (string.Compare(input, "3", true) == 0)
+                        return '3';
+                    else if (string.Compare(input, "4", true) == 0)
+                        return '4';
+                }
                 // Error
                 Console.WriteLine("Please Choose a valid option");
             }
         }
-        
+           
+        // keeping the information the user gives us
+        static string Name;
+        static string Description;
+        static decimal Time;
+        static bool Own;
+            
         
         
         
