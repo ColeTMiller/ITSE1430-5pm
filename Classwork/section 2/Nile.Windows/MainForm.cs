b5063f74abd273f9ca4e2e20c33d8d4308f7d393
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-18 18:22:54 -0500

    move class work to folders

---
+++ Classwork/section 2/Nile.Windows/MainForm.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * Cole Miller
+ * class Example 
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Nile.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
