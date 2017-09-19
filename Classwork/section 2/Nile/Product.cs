a6633760ac4decb6d9f0231869d6fd8c7571d5c7
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-18 19:10:21 -0500

    reupdate

--- Classwork/section 2/Nile/Product.cs
+++ Classwork/section 2/Nile/Product.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace Nile
 {
+    /// <summary> Represents a product.</summary>
+    /// <remarks>this will represent a product with other stuff. </remarks>
    public class Product
     {
+        /// <summary>Gets or sets the name </summary>
        public string Name;
+        /// <summary>Gets or sets the description </summary>
        public string Description;
+        /// <summary>Gets or sets the price  </summary>
        public decimal Price;
+        /// <summary>Determines if discontinued</summary>
        public bool IsDiscontinued;
     }
 }
