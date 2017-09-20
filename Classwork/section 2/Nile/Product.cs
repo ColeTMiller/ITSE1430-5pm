34b4346519179bc29f8e32345a1aaa5830727073
cole.miller218 <cole.miller218@my.tccd.edu>
2017-09-20 18:51:42 -0500

    class work update

--- Classwork/section 2/Nile/Product.cs
+++ Classwork/section 2/Nile/Product.cs
@@ -8,15 +8,67 @@ namespace Nile
 {
     /// <summary> Represents a product.</summary>
     /// <remarks>this will represent a product with other stuff. </remarks>
-   public class Product
+    public class Product
     {
+        public readonly Product None = new Product();
+
         /// <summary>Gets or sets the name </summary>
-       public string Name;
+        /// <value>Never returns null.</value>
+        public string Name
+        {
+            // string get_Name()
+            get
+            {
+                return _name ?? "";
+            }
+            // void set_Name( string value )
+            set
+            {
+                _name = value?.Trim();
+                ;
+            }
+        }
+
         /// <summary>Gets or sets the description </summary>
-       public string Description;
+        public string Description
+        {
+            get
+            {
+                return _description ?? " ";
+            }
+            set
+            {
+                _description = value?.Trim();
+            }
+        }
         /// <summary>Gets or sets the price  </summary>
-       public decimal Price;
+        public decimal Price { get; set; } = 0;
+
+        public const decimal DiscontinuedDiscountRate = 0.10M;
         /// <summary>Determines if discontinued</summary>
-       public bool IsDiscontinued;
+        public bool IsDiscontinued { get; set; }
+
+
+        /// <summary>gets the discounted price, if applicabel. </summary>
+        public decimal DiscuntedPrice
+        {
+            get
+            {
+                ///this.
+                if (IsDiscontinued)
+                    return Price * DiscontinuedDiscountRate;
+
+                return Price;
+            }
+        }
+
+        public int ICanOnlySetIt { get; private set; }
+        public int ICanOnlySetIt2 { get; }
+
+        private string _name;
+        private string _description;
+
+        private readonly double _someValueICannotChange = 10; 
+
     }
 }
