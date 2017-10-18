36c200833ecb2c5b6f807d2c052d8a2dc0bfa68c
cole.miller218 <cole.miller218@my.tccd.edu>
2017-10-18 17:08:11 -0500

    maybe

--- Classwork/Section2/Nile/ProductDatabase.cs
+++ Classwork/Section2/Nile/ProductDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,23 +12,36 @@ namespace Nile
     {
         public ProductDatabase ()
         {
-            _products[0] = new Product();
-            _products[0].Name = "Galaxy S7";
-            _products[0].Price = 650;
+            //Long way
+            //var product = new Product();
+            //product.Name = "Samsung Note 7";
+            //product.Price = 150;
+            //product.IsDiscontinued = true;
+            //Add(product);
 
-            _products[1] = new Product();
-            _products[1].Name = "Samsung Note 7";
-            _products[1].Price = 150;
-            _products[1].IsDiscontinued = true;
+            //Object initializer syntax
+            //_products.Add(new Product() { Id = 1, Name = "Galaxy S7", Price = 650 });
+            //_products.Add(new Product() { Id = 2, Name = "Galaxy Note 7", Price = 150, IsDiscontinued = true });
+            //_products.Add(new Product() { Id = 3, Name = "Windows Phone", Price = 100 });
+            //_products.Add(new Product() { Id = 4, Name = "iPhone X", Price = 1900, IsDiscontinued = true });
 
-            _products[2] = new Product();
-            _products[2].Name = "Windows Phone";
-            _products[2].Price = 100;
+            //Collection initializer syntax
+            //_products = new List<Product>() { 
+            //    new Product() { Id = 1, Name = "Galaxy S7", Price = 650 },
+            //    new Product() { Id = 2, Name = "Galaxy Note 7", Price = 150, IsDiscontinued = true },
+            //    new Product() { Id = 3, Name = "Windows Phone", Price = 100 },
+            //    new Product() { Id = 4, Name = "iPhone X", Price = 1900, IsDiscontinued = true },
+            //};
 
-            _products[3] = new Product();
-            _products[3].Name = "iPhone X";
-            _products[3].Price = 1900;
-            _products[3].IsDiscontinued = true;
+            //Collection initializer syntax with array
+            _products.AddRange(new [] {
+                new Product() { Id = 1, Name = "Galaxy S7", Price = 650 },
+                new Product() { Id = 2, Name = "Galaxy Note 7", Price = 150, IsDiscontinued = true },
+                new Product() { Id = 3, Name = "Windows Phone", Price = 100 },
+                new Product() { Id = 4, Name = "iPhone X", Price = 1900, IsDiscontinued = true },
+            });            
+
+            _nextId = _products.Count + 1;
         }
 
         /// <summary>Adds a product.</summary>
@@ -35,39 +49,90 @@ namespace Nile
         /// <returns>The added product.</returns>
         public Product Add ( Product product )
         {
+            //TODO: Validate
+            if (product == null)
+                return null;
+
+            //Using IValidatableObject
+            if (!ObjectValidator.TryValidate(product, out var errors))
+                return null;
+            //if (!String.IsNullOrEmpty(product.Validate()))
+            //    return null;
+
+            //Emulate database by storing copy
+            var newProduct = CopyProduct(product);
+            _products.Add(newProduct);
+            newProduct.Id = _nextId++;
+
+            return CopyProduct(newProduct);
+
+            //var item = _list[0];
+
             //TODO: Implement Add
-            return product;
+            //return product;
         }
 
         /// <summary>Get a specific product.</summary>
         /// <returns>The product, if it exists.</returns>
-        public Product Get ()
+        public Product Get ( int id )
         {
-            //TODO: Implement Get
-            return null;
+            //TODO: Validate
+            if (id <= 0)
+                return null;
+
+            var product = FindProduct(id);
+
+            return (product != null) ? CopyProduct(product) : null;
         }
 
         /// <summary>Gets all products.</summary>
         /// <returns>The products.</returns>
         public Product[] GetAll ()
         {
-            var items = new Product[_products.Length];
+            var items = new Product[_products.Count];
             var index = 0;
-
             foreach (var product in _products)
-            {
-                //product = new Product();
                 items[index++] = CopyProduct(product);
-            };
 
             return items;
+            //How many products?
+            //var count = 0;
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        ++count;
+            //};
+
+            //var items = new Product[count];
+            //var index = 0;
+
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        //product = new Product();
+            //        items[index++] = CopyProduct(product);
+            //};
+
+            //return items;
         }
 
         /// <summary>Removes the product.</summary>
         /// <param name="product">The product to remove.</param>
-        public void Remove ( Product product )
+        public void Remove ( int id )
         {
-            //TODO: Implement Remove
+            //TODO: Validate
+            if (id <= 0)
+                return;
+
+            var product = FindProduct(id);
+            if (product != null)
+                _products.Remove(product);
+
+            //if (_list[index].Name == product.Name)
+            //{
+            //    _list.RemoveAt(index);
+            //    break;
+            //};        
         }
 
         /// <summary>Updates a product.</summary>
@@ -75,8 +140,28 @@ namespace Nile
         /// <returns>The updated product.</returns>
         public Product Update ( Product product )
         {
-            //TODO: Implement Update
-            return product;
+            //TODO: Validate
+            if (product == null)
+                return null;
+
+            //Using IValidatableObject
+            if (!ObjectValidator.TryValidate(product, out var errors))
+                return null;
+            //if (!String.IsNullOrEmpty(product.Validate()))
+            //    return null;
+
+            //Get existing product
+            var existing = FindProduct(product.Id);
+            if (existing == null)
+                return null;
+
+            //Replace 
+            _products.Remove(existing);
+            
+            var newProduct = CopyProduct(product);
+            _products.Add(newProduct);
+
+            return CopyProduct(newProduct);
         }
 
         private Product CopyProduct ( Product product )
@@ -85,6 +170,7 @@ namespace Nile
                 return null;
 
             var newProduct = new Product();
+            newProduct.Id = product.Id;
             newProduct.Name = product.Name;
             newProduct.Price = product.Price;
             newProduct.IsDiscontinued = product.IsDiscontinued;
@@ -92,6 +178,21 @@ namespace Nile
             return newProduct;
         }
 
-        private Product[] _products = new Product[100];
+        //Find a product by ID
+        private Product FindProduct ( int id )
+        {
+            foreach (var product in _products)
+            {
+                if (product.Id == id)
+                    return product;
+            };
+
+            return null;
+        }
+
+        //private Product[] _products = new Product[100];
+        private List<Product> _products = new List<Product>();
+        private int _nextId = 1;
+        //private List<int> _ints;
     }
 }
